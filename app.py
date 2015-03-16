441b02c0d978f4e4504ace3747d8f412318bed28
Damien Casoni <damiencasoni@gmail.com>
2015-03-16 17:33:03 +1100

    Status code should be explicit : 200 OK, 404 not found, etc

--- app.py
+++ app.py
@@ -19,6 +19,32 @@ def hello_world():
 def search(search_query):
     return search_query
     
+@app.route("/integer/<int:value>")
+def int_type(value):
+    print value + 1
+    return "correct"
+    
+@app.route("/float/<float:value>")
+def float_type(value):
+    print value + 1
+    return "correct"  # je ne sais pas pourquoi pas impossible de return value
+    
+# dynamic route that accepts slashes
+@app.route("/path/<path:value>")
+def path_type(value):
+    print value
+    return "correct"
+    
+@app.route("/name/<name>")
+def index(name):
+    if name.lower() == "michael":
+        return "Hello {}".format(name)  #, 200 Status codeis optional, 
+                                        # Flask is smart
+                                        # it's convention anyway to explicitely
+                                        # define Status Code for RESTful APIs
+    else:
+        return "Not found", 404
+    
 # start the development server using the run() method
 if __name__ == "__main__":
     app.run()
